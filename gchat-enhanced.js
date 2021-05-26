d06d3f168e48f3b17d2903642a4d5380bc6ea284
CristiG87 <petrut.grigore@yahoo.com>
2021-05-26 11:17:17 +0300

    Update gchat-enhanced.js

--- gchat-enhanced.js
+++ gchat-enhanced.js
@@ -1,3 +1,4 @@
+// ==UserScript==
 // @name         GChat Enhanced
 // @version      1.0
 // @description  Extended functions for Google Chat
@@ -8,3 +9,4 @@
 // @icon         https://www.google.com/s2/favicons?domain=chat.google.com
 // @source       https://github.com/CristiG87/gchat-enhanced
 // @require      https://raw.githubusercontent.com/CristiG87/gchat-enhanced/main/script.js
+// /==UserScript==
