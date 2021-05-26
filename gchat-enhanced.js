1c35faedc51b96707a1f7113fd572bc98a8a4296
CristiG87 <petrut.grigore@yahoo.com>
2021-05-26 11:53:44 +0300

    Update gchat-enhanced.js

--- gchat-enhanced.js
+++ gchat-enhanced.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GChat Enhanced
-// @version      1.0
+// @version      1.0.1
 // @description  Extended functions for Google Chat
 // @author       CristiG87
 // @grant        none
